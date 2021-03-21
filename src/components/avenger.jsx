97b3b37d67ecde041ca21570cf09f7fafe4feea9
Udara Sachinthana <u4dara@gmail.com>
2021-03-21 19:42:41 +0530

    initial commit

---
+++ src/components/avenger.jsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+class Avenger extends Component {
+    render() {
+        return <h1>Avenger Incoming.....</h1>;
+    }
+}
+
+export default Avenger;
