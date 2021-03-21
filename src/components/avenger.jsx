0ade0cf8f5c5e45b982c9453d47ee20a8d98ef47
Udara Sachinthana <u4dara@gmail.com>
2021-03-21 22:12:18 +0530

    Card property done

--- src/components/avenger.jsx
+++ src/components/avenger.jsx
@@ -2,9 +2,54 @@ import { render } from "@testing-library/react";
 import React, { Component } from "react";
 
 class Avenger extends Component {
-    render() {
-        return <h1>Avenger Incoming.....</h1>;
-    }
+    /*This is class component and also you can use function component
+    every component should have render() */
+
+    // state = {
+    //     avengerID: 10000
+    // };
+    // isAvenger() {
+    //     return this.state.avengerID > 1000 ? "He is avenger" : "Not an avenger";
+    // }
+    // render() {
+    //     return ( //can use <div> or <React.Fragment>
+    //         <React.Fragment> 
+    //             <h1>Avenger Incoming.....</h1>
+    //             <button className="btn btn-primary">Click Here {this.state.avengerID}</button>
+    //             <p style={{color:"red", fontSize:"20px"}}>{this.isAvenger()}</p>
+    //         </React.Fragment>
+    //     );
+    // }
+
+  state = {
+    likeCount: 0,
+    movies: ["First Avenger", "Iron Man", "Endgame", "Infinity War"]
+  };
+  showMovies() {
+    if(this.state.movies.length == 0)
+    return <p>No movies available</p>;
+    return this.state.movies.map((movie, index) => (<li key={index}>{movie}</li>))
+  }
+  likeAvenger() {
+    console.log("Like the avenger");
+  }
+  render() {
+    return (
+      <div className="card" style={{width: "18rem"}}>
+        <img src="https://p1.hiclipart.com/preview/878/208/854/avengers-endgame-2019-avengers-logo-avengers-logo-png-clipart.jpg" className="card-img-top" alt="..."></img>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <ul>
+            {this.showMovies()}
+          </ul>
+          <button className="btn btn-primary" onClick={this.likeAvenger()}>Like</button>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default Avenger;
